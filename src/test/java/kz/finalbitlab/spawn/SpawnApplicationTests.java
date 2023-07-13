942555a6a5b0ecf8a2b35dc6bf1a8a411f43b377
Doни <daniyar.doskulov@narxoz.kz>
2023-07-14 03:10:44 +0600

    "Unit Completed"

--- src/test/java/kz/finalbitlab/spawn/SpawnApplicationTests.java
+++ src/test/java/kz/finalbitlab/spawn/SpawnApplicationTests.java
@@ -3,6 +3,7 @@ package kz.finalbitlab.spawn;
 import kz.finalbitlab.spawn.dto.GameDTO;
 import kz.finalbitlab.spawn.mapper.GameMapper;
 import kz.finalbitlab.spawn.model.Game;
+import kz.finalbitlab.spawn.service.GameService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,9 @@ class SpawnApplicationTests {
 
 	@Autowired
 	private GameMapper gameMapper;
+
+	@Autowired
+	private GameService gameService;
 
 	@Test
 	void contextLoads() {
@@ -27,5 +31,16 @@ class SpawnApplicationTests {
 		Assertions.assertEquals(game.getYears(),gameDTO.getYears());
 		Assertions.assertEquals(game.getImage(),gameDTO.getImage());
 	}
+	void checkInsertIntoDb(){
+		Game game = new Game();
+		GameDTO newGame = gameService.addGame(gameMapper.toDto(game));
+		Assertions.assertNotNull(newGame);
+		Assertions.assertNotNull(newGame.getId());
+		Assertions.assertEquals(game.getName(),newGame.getName());
+		Assertions.assertEquals(game.getYears(),newGame.getYears());
+		Assertions.assertEquals(game.getImage(),newGame.getImage());
+		Assertions.assertEquals(game.getDescription(),newGame.getDescription());
+		gameService.deleteGame(game.getId());
 
+	}
 }
